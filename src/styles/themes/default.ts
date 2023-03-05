339b2e487f87c65537bc0743e73745111f013e1c
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-03-05 19:21:41 -0300

    Novas imagens

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,5 +1,21 @@
 export const defaultTheme = {
+  background: '#FAFAFA',
   white: '#fff',
 
-  'gray-100': '#E1E1E6',
+  'yellow-dark': '#C47F17',
+  yellow: '#DBAC2C',
+  'yellow-light': '#F1E9C9',
+
+  'purple-dark': '#4B2995',
+  purple: '#8047F8',
+  'purple-light': '#EBE5F9',
+
+  'base-title': '#272221',
+  'base-subtitle': '#403937',
+  'base-text': '#574F4D',
+  'base-label': '#8D8686',
+  'base-hover': '#D7D5D5',
+  'base-button': '#E6E5E5',
+  'base-input': '#EDEDED',
+  'base-card': '#F3F2F2',
 } as const
