f0fa833f64a8b69611c01804d07981ff5d565af8
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-03-08 22:58:59 -0300

    Mudanças coffee card

--- src/pages/Home/components/CoffeeCard/index.tsx
+++ src/pages/Home/components/CoffeeCard/index.tsx
@@ -1,18 +1,22 @@
 import { ShoppingCart } from 'phosphor-react'
-import { CoffeeCardContainer } from './styles'
+import {
+  CoffeeCardContainer,
+  CoffeeCardContent,
+  CoffeeCardOption,
+} from './styles'
 
 export function CoffeeCard() {
   return (
     <CoffeeCardContainer>
-      <div>
+      <CoffeeCardContent>
         <img src="/coffees/coffee-expresso.png" alt="" />
         <span>Tradicional</span>
-      </div>
-      <div>
+      </CoffeeCardContent>
+      <CoffeeCardContent>
         <h1>Expresso Tradicional</h1>
         <span>O tradicional café feito com água quente e grãos moídos</span>
-      </div>
-      <div>
+      </CoffeeCardContent>
+      <CoffeeCardOption>
         <span>R$ 9,90</span>
         <div>
           <button>-</button>
@@ -20,7 +24,7 @@ export function CoffeeCard() {
           <button>+</button>
         </div>
         <ShoppingCart size={28} weight="fill" />
-      </div>
+      </CoffeeCardOption>
     </CoffeeCardContainer>
   )
 }
--- src/pages/Home/components/CoffeeCard/styles.ts
+++ src/pages/Home/components/CoffeeCard/styles.ts
@@ -9,7 +9,18 @@ export const CoffeeCardContainer = styled.div`
   left: 0px;
   top: 0px;
 
-  /* Base/Card */
   background: ${(props) => props.theme['base-card']};
   border-radius: 0.375rem 2.25rem;
+`
+
+export const CoffeeCardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const CoffeeCardOption = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `
